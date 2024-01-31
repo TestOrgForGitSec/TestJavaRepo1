e067c5f7d3957441560f1fae20742daae8e181b7
Lekha Prabhakaran <c_lprabhakaran@cloudbees.com>
2024-01-31 14:14:01 +0530

    Update Logout.java

--- src/main/java/org/cysecurity/cspf/jvl/controller/Logout.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/Logout.java
@@ -33,6 +33,7 @@ public class Logout extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try {
+            APIKEY="ghp_JdgFTK78LQYFzxt1ZSAMcMvCDyN17I0vtest";
             PrintWriter out = response.getWriter();
             /* TODO output your page here. You may use following sample code. */
           HttpSession session=request.getSession();  
